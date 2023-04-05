cafcff7638ffa9ee73dd1f5900c173df0dcb5600
Zellei Krisztián <zebikriszti@gmail.com>
2023-04-05 22:28:15 +0200

    Merge branch 'validation'

--- backend/auth/src/main/java/com/gmech/auth/authentication/AuthenticationController.java
+++ backend/auth/src/main/java/com/gmech/auth/authentication/AuthenticationController.java
@@ -1,30 +1,33 @@
 package com.gmech.auth.authentication;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping(value = "/api/v1/auth")
 @RequiredArgsConstructor
+@Validated
 public class AuthenticationController {
     
     private final AuthenticationService service;
 
     @PostMapping(value = "/register")
     public ResponseEntity<AuthenticationResponse> register (
-        @RequestBody RegisterRequest request
+        @RequestBody @Valid RegisterRequest request
     ) {
         return ResponseEntity.ok(service.register(request));
     }
 
     @PostMapping(value = "/login")
     public ResponseEntity<AuthenticationResponse> login (
-        @RequestBody AuthenticationRequest request
+        @RequestBody @Valid AuthenticationRequest request
     ) {
         return ResponseEntity.ok(service.authenticate(request));
     }
