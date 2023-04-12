c52d19ce0a652483b2540f9e94a94d55b1d5bc31
csabaialbert <csabaialbert@gmail.com>
2023-04-12 18:24:53 +0200

    Debugging stock delete method

--- backend/stock/src/main/java/com/gmech/stock/service/StockService.java
+++ backend/stock/src/main/java/com/gmech/stock/service/StockService.java
@@ -12,71 +12,75 @@ import com.gmech.stock.stock.Stock;
 import com.gmech.stock.stock.StockRepository;
 import com.gmech.stock.stock.StockRequest;
 import com.gmech.stock.stock.StockResponse;
+
+import jakarta.transaction.Transactional;
+
 import com.gmech.stock.exception.DuplicateException;
 import com.gmech.stock.exception.IncorrectMatnumberException;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class StockService {
 
-    private final StockRepository stockRepository;
+        private final StockRepository stockRepository;
 
-    @Autowired
-    private final ModelMapper modelMapper;
+        @Autowired
+        private final ModelMapper modelMapper;
 
-    public StockResponse create(StockRequest request) {
-        stockRepository.findByMaterialNumber(request.getMaterialNumber()).ifPresent(c -> {
-            throw new DuplicateException("A megadott cikkszámmal már rendelkezik jármű!");
-        });
+        public StockResponse create(StockRequest request) {
+                stockRepository.findByMaterialNumber(request.getMaterialNumber()).ifPresent(c -> {
+                        throw new DuplicateException("A megadott cikkszámmal már rendelkezik jármű!");
+                });
 
-        var stock = Stock.builder()
-                .materialNumber(request.getMaterialNumber())
-                .description(request.getDescription())
-                .currentStock(request.getCurrentStock())
-                .netPrice(request.getNetPrice())
-                .build();
+                var stock = Stock.builder()
+                                .materialNumber(request.getMaterialNumber())
+                                .description(request.getDescription())
+                                .currentStock(request.getCurrentStock())
+                                .netPrice(request.getNetPrice())
+                                .build();
 
-        return this.modelMapper.map(
-                stockRepository.save(stock),
-                StockResponse.class);
-    }
+                return this.modelMapper.map(
+                                stockRepository.save(stock),
+                                StockResponse.class);
+        }
 
-    public StockResponse get(String materialNumber) {
-        var stock = stockRepository.findByMaterialNumber(materialNumber)
-                .orElseThrow(() -> new IncorrectMatnumberException("A megadott aznosító nem létezik!"));
-        return this.modelMapper.map(
-                stock,
-                StockResponse.class);
-    }
+        public StockResponse get(String materialNumber) {
+                var stock = stockRepository.findByMaterialNumber(materialNumber)
+                                .orElseThrow(() -> new IncorrectMatnumberException("A megadott aznosító nem létezik!"));
+                return this.modelMapper.map(
+                                stock,
+                                StockResponse.class);
+        }
 
-    public List<StockResponse> getAll() {
-        var stocks = stockRepository.findAll();
+        public List<StockResponse> getAll() {
+                var stocks = stockRepository.findAll();
 
-        return stocks.stream().map((stock) -> modelMapper.map(stock, StockResponse.class))
-                .collect(Collectors.toList());
+                return stocks.stream().map((stock) -> modelMapper.map(stock, StockResponse.class))
+                                .collect(Collectors.toList());
 
-    }
+        }
 
-    public StockResponse put(StockRequest request) {
+        public StockResponse put(StockRequest request) {
 
-        var stock = stockRepository.findByMaterialNumber(request.getMaterialNumber())
-                .orElseThrow(() -> new IncorrectMatnumberException("A megadott aznosító nem létezik!"));
+                var stock = stockRepository.findByMaterialNumber(request.getMaterialNumber())
+                                .orElseThrow(() -> new IncorrectMatnumberException("A megadott aznosító nem létezik!"));
 
-        stock.setDescription(request.getDescription());
-        stock.setCurrentStock(request.getCurrentStock());
-        stock.setNetPrice(request.getNetPrice());
+                stock.setDescription(request.getDescription());
+                stock.setCurrentStock(request.getCurrentStock());
+                stock.setNetPrice(request.getNetPrice());
 
-        stockRepository.save(stock);
-        return this.modelMapper.map(
-                stock,
-                StockResponse.class);
+                stockRepository.save(stock);
+                return this.modelMapper.map(
+                                stock,
+                                StockResponse.class);
 
-    }
+        }
 
-    public void delete(String materialNumber) {
-        stockRepository.findByMaterialNumber(materialNumber)
-                .orElseThrow(() -> new IncorrectMatnumberException("A megadott aznosító nem létezik!"));
+        public void delete(String materialNumber) {
+                stockRepository.findByMaterialNumber(materialNumber)
+                                .orElseThrow(() -> new IncorrectMatnumberException("A megadott aznosító nem létezik!"));
 
-        stockRepository.deleteByMaterialNumber(materialNumber);
-    }
+                stockRepository.deleteByMaterialNumber(materialNumber);
+        }
 }
--- backend/stock/src/main/java/com/gmech/stock/stock/StockRepository.java
+++ backend/stock/src/main/java/com/gmech/stock/stock/StockRepository.java
@@ -3,9 +3,12 @@ package com.gmech.stock.stock;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import jakarta.transaction.Transactional;
+
+@Transactional
 public interface StockRepository extends JpaRepository<Stock, String> {
 
     Optional<Stock> findByMaterialNumber(String materialNumber);
 
-    Long deleteByMaterialNumber(String materialNumber);
+    Optional<Stock> deleteByMaterialNumber(String materialNumber);
 }
