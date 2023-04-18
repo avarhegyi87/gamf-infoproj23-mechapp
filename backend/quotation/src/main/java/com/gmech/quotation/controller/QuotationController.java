d2771fb656e0b9bf003f8b2eaacf03bb619ed087
steviewhisper <zebikriszti@gmail.com>
2023-04-18 08:27:11 +0200

    Added missing endpoints to quotation

--- backend/quotation/src/main/java/com/gmech/quotation/controller/QuotationController.java
+++ backend/quotation/src/main/java/com/gmech/quotation/controller/QuotationController.java
@@ -1,11 +1,18 @@
 package com.gmech.quotation.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.DeleteExchange;
 
 import com.gmech.quotation.quotation.QuotationRequest;
 import com.gmech.quotation.quotation.QuotationResponse;
@@ -13,6 +20,9 @@ import com.gmech.quotation.service.QuotationService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping(value = "/api/v1/quotation")
@@ -27,5 +37,26 @@ public class QuotationController {
         @RequestBody @Valid QuotationRequest request
     ) {
         return ResponseEntity.ok(service.create(request));
+    }
+
+    @GetMapping(value = "/get")
+    public ResponseEntity<QuotationResponse> get(@RequestParam Integer id) {
+        return ResponseEntity.ok(service.get(id));
+    }
+
+    @GetMapping(value="/getAll")
+    public ResponseEntity<List<QuotationResponse>> getAll() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @PutMapping(value = "/put")
+    public ResponseEntity<QuotationResponse> put(@RequestBody @Valid QuotationRequest request) {
+        return ResponseEntity.ok(service.put(request));
+    }
+    
+    @DeleteMapping(value = "/delete")
+    public ResponseEntity<Void> delete(@RequestParam Integer id) {
+        service.delete(id);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 }
