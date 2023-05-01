2f49b601d32675507bd60e3c1d1dce072adf83ae
Ádám Várhegyi-Miłoś <42640245+avarhegyi87@users.noreply.github.com>
2023-05-01 21:07:20 +0200

    feat: access levels in navbar, dark theme (#35)

--- frontend/src/app/modules/users/services/user.service.ts
+++ frontend/src/app/modules/users/services/user.service.ts
@@ -2,12 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user.model';
 import { environment } from 'src/environments/environment';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
+  private currentUserSubject = new BehaviorSubject<any>(null);
+
   constructor(private http: HttpClient) {}
 
   getAll() {
     return this.http.get<User[]>(`${environment.baseApiUrl}/api/users`);
+  }
+
+  getCurrentUser() {
+    return this.currentUserSubject.asObservable();
   }
 }
