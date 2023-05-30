fb44ac1610a6ac62d27b12e4001f739ea546ae00
csabaialbert <csabaialbert@gmail.com>
2023-05-30 20:27:38 +0200

    feat-customers-vehicles-debug

---
+++ frontend/src/app/modules/vehicle/models/vehicleApi.model.ts
@@ -0,0 +1,14 @@
+
+
+export interface VehicleApi {
+  id: number;
+  vin: string;
+  licencePlate: string;
+  customerId: number;
+  productionYear: number;
+  mileage: number;
+  carBrand: string;
+  carMake: string;
+  displacement: number;
+  fuelType: string;
+}
--- frontend/src/app/modules/vehicle/services/vehicle.service.ts
+++ frontend/src/app/modules/vehicle/services/vehicle.service.ts
@@ -2,36 +2,37 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Vehicle } from '../models/vehicle.model';
+import { VehicleApi } from '../models/vehicleApi.model';
 import { Injectable } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class VehicleService {
   baseApiUrl: string = environment.baseApiUrl;
-  entityName = 'vehicle';
+  entityName = '/vehicle';
 
   constructor(private http: HttpClient) {}
 
-  getAllVehicles(): Observable<Vehicle[]> {
-    return this.http.get<Vehicle[]>(this.baseApiUrl + this.entityName + '/getall');
+  getAllVehicles(): Observable<VehicleApi[]> {
+    return this.http.get<VehicleApi[]>(this.baseApiUrl + this.entityName + '/getAll');
   }
 
   getVehiclesByCustomer(id: number): Observable<Vehicle[]> {
     return this.http.get<Vehicle[]>(this.baseApiUrl + this.entityName + '/getallbycust?' + id);
   }
 
-  getVehicle(id: number): Observable<Vehicle> {
-    return this.http.get<Vehicle>(this.baseApiUrl + this.entityName + '/get?' + id);
+  getVehicle(id: number): Observable<VehicleApi> {
+    return this.http.get<VehicleApi>(this.baseApiUrl + this.entityName + '/get/' + id);
   }
 
-  addVehicle(addVehicleRequest: Vehicle): Observable<Vehicle> {
-    return this.http.post<Vehicle>(this.baseApiUrl + this.entityName + '/create', addVehicleRequest);
+  addVehicle(addVehicleRequest: VehicleApi): Observable<VehicleApi> {
+    return this.http.post<VehicleApi>(this.baseApiUrl + this.entityName + '/create', addVehicleRequest);
   }
 
-  updateVehicle(id: number, updateVehicleRequest: Vehicle): Observable<Vehicle> {
-    return this.http.put<Vehicle>(this.baseApiUrl + this.entityName + '/put?' + id, updateVehicleRequest);
+  updateVehicle(id: number, updateVehicleRequest: VehicleApi): Observable<VehicleApi> {
+    return this.http.post<VehicleApi>(this.baseApiUrl + this.entityName + '/put/' + id, updateVehicleRequest);
   }
 
   deleteVehicle(id: number): Observable<Vehicle> {
-    return this.http.delete<Vehicle>(this.baseApiUrl + this.entityName + '/delete?' + id);
+    return this.http.delete<Vehicle>(this.baseApiUrl + this.entityName + '/delete/' + id);
   }
 }
