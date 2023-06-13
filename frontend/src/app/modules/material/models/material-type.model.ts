5fc6a8ca7e35ac4d2200c7f3ec3b4e7229f831ff
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2023-06-13 20:12:24 +0200

    feat: get material type from id

--- frontend/src/app/modules/material/models/material-type.model.ts
+++ frontend/src/app/modules/material/models/material-type.model.ts
@@ -8,10 +8,18 @@ export const MaterialTypeToLabelMapping: Record<MaterialTypeEnum, string> = {
   [MaterialTypeEnum.service]: 'Szolgáltatás',
 }
 
-export function findEnumByLabel(label: string): MaterialTypeEnum | undefined {
+export function findMatTypeByLabel(label: string): MaterialTypeEnum | undefined {
   for (const key of Object.keys(MaterialTypeToLabelMapping)) {
     if (MaterialTypeToLabelMapping[key as MaterialTypeEnum] === label)
       return key as MaterialTypeEnum;
   }
   return undefined;
+}
+
+export function findMatTypeByNumber(matNum: string): MaterialTypeEnum | undefined {
+  switch (true) {
+    case matNum.startsWith('1'): return MaterialTypeEnum.material;
+    case matNum.startsWith('6'): return MaterialTypeEnum.service;
+    default: return undefined;
+  }
 }
