b75f3e9a3227306bfd0d0ee5738a835411dcc024
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2023-06-26 01:55:10 +0200

    adding worksheet - incomplete

--- frontend/src/app/modules/worksheet/components/add-worksheet/add-worksheet.component.ts
+++ frontend/src/app/modules/worksheet/components/add-worksheet/add-worksheet.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable, map, startWith } from 'rxjs';
 import { Customer } from 'src/app/modules/customer/models/customer.model';
 import { CustomerService } from 'src/app/modules/customer/services/customer.service';
+import { Job } from 'src/app/modules/job/models/job.model';
+import { JobService } from 'src/app/modules/job/services/job.service';
 import { Material } from 'src/app/modules/material/models/material.model';
 import { MaterialService } from 'src/app/modules/material/services/material.service';
+import { QuotationJobList } from 'src/app/modules/quotation/models/quotation-job-list.model';
 import { Quotation } from 'src/app/modules/quotation/models/quotation.model';
 import { QuotationService } from 'src/app/modules/quotation/services/quotation.service';
 import { User } from 'src/app/modules/users/models/user.model';
 import { AuthenticationService } from 'src/app/modules/users/services/authentication.service';
 import { Vehicle } from 'src/app/modules/vehicle/models/vehicle.model';
 import { VehicleService } from 'src/app/modules/vehicle/services/vehicle.service';
+import { DynamicTableComponent } from 'src/app/shared/components/dynamic-table/dynamic-table.component';
 
 @Component({
   selector: 'app-add-worksheet',
@@ -29,23 +33,35 @@ export class AddWorksheetComponent implements OnInit {
   customers: Customer[] = [];
   filteredCustomers!: Observable<Customer[]>;
   customerControl = new FormControl<string | Customer>('');
+  selectedCustomer: Customer | undefined;
+  isCustomerSelected = false;
 
   // properties for filtering Vehicles in the auto-complete field of addWorksheetForm
   vehicles: Vehicle[] = [];
   filteredVehicles!: Observable<Vehicle[]>;
   vehicleControl = new FormControl<string | Vehicle>('');
-  customerSelected = false;
+  selectedVehicle: Vehicle | undefined;
+  isVehicleSelected = false;
 
-  // properties for filtering Quotations in the auto-complete field of addWorksheetForm
+  // properties for Quotations
   quotations: Quotation[] = [];
   filteredQuotations!: Observable<Quotation[]>;
   quotationControl = new FormControl<string | Quotation>('');
+  selectedQuotation: Quotation | undefined;
+  jobsOnQuotation: Job[] = [];
+
+  // properties for Jobs
 
   // properties for filtering Materials in the auto-complete field of addWorksheetForm
   addMaterialForm!: FormGroup;
   materials: Material[] = [];
   filteredMaterials!: Observable<Material[]>;
   materialControl = new FormControl<string | Material>('');
+  materialToAdd!: Material;
+  materialsAdded: QuotationJobList[] = [];
+
+  // DynamicTableComponent for adding materials
+  @ViewChildren(DynamicTableComponent) dynamicTable!: DynamicTableComponent;
 
   // other properties
   error = '';
@@ -57,6 +73,7 @@ export class AddWorksheetComponent implements OnInit {
     private customerService: CustomerService,
     private vehicleService: VehicleService,
     private materialService: MaterialService,
+    private jobService: JobService,
     private authService: AuthenticationService,
   ) {
     this.authService.getCurrentUser.subscribe(user => this.currentUser = user);
@@ -115,14 +132,6 @@ export class AddWorksheetComponent implements OnInit {
       }),
     );
 
-    /* this.filteredQuotations = this.quotationControl.valueChanges.pipe(
-      startWith(''),
-      map(value => {
-        const quotationNumber = typeof value === 'number' ? value : value?.toString;
-        return quotationNumber ? this._
-      })
-    ); */
-
     this.filteredMaterials = this.materialControl.valueChanges.pipe(
       startWith(''),
       map(value => {
@@ -161,9 +170,13 @@ export class AddWorksheetComponent implements OnInit {
   }
 
   onCustomerOptionSelected(customer: Customer): void {
+    this.selectedCustomer = customer;
+    this.isCustomerSelected = true;
     this.vehicleService.getVehiclesByCustomer(customer.id).subscribe(vehicles => {
       this.vehicles = vehicles;
-      this.customerSelected = true;
+    });
+    this.quotationService.getByCustomerId(customer.id).subscribe(q => {
+      this.quotations = q;
     });
   }
 
@@ -172,8 +185,83 @@ export class AddWorksheetComponent implements OnInit {
   }
 
   onVehicleOptionSelected(vehicle: Vehicle): void {
+    this.selectedVehicle = vehicle;
+    this.isVehicleSelected = true;
     this.quotationService.getByVehicleId(vehicle.id).subscribe(quotations => {
       this.quotations = quotations;
-    })
+    });
+  }
+
+  displayVehicle(vehicle: Vehicle): string {
+    return vehicle.licencePlate || '';
+  }
+
+  onQuotationOptionSelected(event: any): void {
+    this.selectedQuotation = event.value;
+    if (this.selectedQuotation) {
+      this.jobsOnQuotation = [];
+      this.jobService.getByQuotationId(this.selectedQuotation.id).subscribe(jobs => {
+        this.jobsOnQuotation = jobs;
+      });
+      this.jobsOnQuotation.forEach(job => {
+        this.materialService.getMaterial(job.materialId.toString()).subscribe(m => {
+          job.materialId = m;
+        });
+      })
+    }
+  }
+
+  getJobMaterialById(id: string): Material | undefined {
+    return this.materials.find(m => m.materialNumber === id);
+  }
+
+  onInputBlur(event: Event, source: string): void {
+    const inputValue = (event.target as HTMLInputElement).value;
+    switch (source) {
+      case 'customer':
+        if (!inputValue) {
+          this.selectedCustomer = undefined;
+          this.isCustomerSelected = false;
+        }
+        break;
+
+      case 'vehicle':
+        if (!inputValue) {
+          this.selectedVehicle = undefined;
+          this.isVehicleSelected = false;
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  onMaterialOptionSelected(material: Material): void {
+    this.materialToAdd = material;
+  }
+
+  addNewMaterial(): void {
+    const newMaterial: QuotationJobList = {
+      materialNumber: this.materialToAdd.materialNumber,
+      description: this.materialToAdd.description,
+      quantity: this.addMaterialForm.get('quantity')?.value,
+      unitPrice: this.materialToAdd.netPrice,
+      subTotal: this.materialToAdd.netPrice * this.addMaterialForm.get('quantity')?.value,
+    };
+    const existingMaterial: QuotationJobList | undefined = this.materialsAdded.find(
+      m => m.materialNumber === newMaterial.materialNumber,
+    );
+    if (existingMaterial) {
+      existingMaterial.quantity += newMaterial.quantity;
+      existingMaterial.subTotal = existingMaterial.unitPrice * existingMaterial.quantity;
+    } else {
+      this.materialsAdded.push(newMaterial);
+    }
+    this.addMaterialForm.reset();
+  }
+
+  displayMaterials(material: Material): string {
+    return material.description || '';
   }
 }
