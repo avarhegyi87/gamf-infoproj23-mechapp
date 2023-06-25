1c403514a2cbe6848d28c8473a7120664478a945
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2023-06-26 01:54:22 +0200

    fix: update job model

--- frontend/src/app/modules/job/models/job.model.ts
+++ frontend/src/app/modules/job/models/job.model.ts
@@ -1,10 +1,11 @@
 import { Material } from '../../material/models/material.model';
 import { Quotation } from '../../quotation/models/quotation.model';
+import { Worksheet } from '../../worksheet/models/worksheet.model';
 
 export interface Job {
   id: number;
   quotationId?: Quotation | number;
-  //worksheetId?: Worksheet | number;
-  materialId: Material | number;
+  worksheetId?: Worksheet | number;
+  materialId: Material | string;
   unit: number;
 }
