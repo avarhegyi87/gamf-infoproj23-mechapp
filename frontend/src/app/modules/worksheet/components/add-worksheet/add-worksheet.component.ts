5b9c5fa82444f01835a92fc9f1cd87b984e2548f
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2023-06-25 17:42:41 +0200

    feat: add-worksheet model, service, empty component

---
+++ frontend/src/app/modules/worksheet/components/add-worksheet/add-worksheet.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-worksheet',
+  templateUrl: './add-worksheet.component.html',
+  styleUrls: ['./add-worksheet.component.scss'],
+})
+export class AddWorksheetComponent implements OnInit {
+
+  constructor() {
+    /**TODO: constructor for AddWorksheetComponent */
+  }
+
+  ngOnInit(): void {
+    /**TODO: ngOnInit for AddWorksheetComponent */
+  }
+
+}
